79dab0c1af78761d30128a69a23461e1c13aa81c
Andrew35780 <andreyka_popov_2004@mail.ru>
2024-11-27 22:24:13 +0300

    Make LR 13

---
+++ LR13/Example1.cpp
@@ -0,0 +1,51 @@
+﻿#include <locale.h>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+/*
+void main()
+{
+	setlocale(LC_CTYPE, "");
+	system("color f0");
+
+	const int N = 3, M = 4;
+	int A[N][M], max[N];
+	int i, j;
+
+	for (i = 0; i < N; i++) // ввод матрицы
+	{
+		for (j = 0; j < M; j++)
+		{
+			cout << " Введите элемент матрицы A = " << endl;
+			cin >> A[i][j];
+		}
+	}
+
+	cout << "\n Вывод  матрицы А : " << endl;
+
+	for (i = 0; i < N; i++) // вывод исходной матрицы по строкам
+	{
+		for (j = 0; j < M; j++)   
+			cout << setw(4) << A[i][j] << "  ";
+
+		cout << endl; // переход на новую строку в матрице
+	}
+
+	for (i = 0; i < N; i++)
+	{
+		max[i] = A[i][0]; // первый элемент в каждой строке полагаем наибольшим
+		for (j = 0; j < M; j++)
+		{
+			if (A[i][j] > max[i])
+				max[i] = A[i][j];
+		}
+	}
+	cout << endl;
+
+	cout << " Вывод массива максимальных элементов в каждой строке : " << endl;
+	for (i = 0; i < N; i++)  
+		cout << max[i] << endl;
+}
+*/
---
+++ LR13/Task1.cpp
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <locale.h>
+#include <iostream>
+
+void InputMatrixSize(int* rows, int* columns);
+void AllocMemoryForMatrix(int*** matrix, int* rows, int* columns);
+void InputMatrix(int** matrix, int* rows, int* columns);
+void PrintMatrix(int** matrix, int* rows, int* columns, const char* matrixTitle = "исходной");
+void FreeMatrixMemory(int** matrix, int* rows, int* columns);
+void CalculateAndPrintMatrixSums(int** matrix, int* rows, int* columns, bool const isRowSum);
+
+void main()
+{
+	setlocale(LC_CTYPE, "");
+	system("color f0");
+
+	int rows, columns;
+	int** matrix = NULL;
+
+	InputMatrixSize(&rows, &columns);
+
+	AllocMemoryForMatrix(&matrix, &rows, &columns);
+
+	printf("\n");
+
+	InputMatrix(matrix, &rows, &columns);
+
+	PrintMatrix(matrix, &rows, &columns);
+	
+	CalculateAndPrintMatrixSums(matrix, &rows, &columns, true);
+
+	printf("\n");
+
+	CalculateAndPrintMatrixSums(matrix, &rows, &columns, false);
+
+	FreeMatrixMemory(matrix, &rows, &columns);
+}
+
+void InputMatrixSize(int* rows, int* columns) 
+{
+	printf("\n Введите размерность матрицы (n m): ");
+	scanf_s("%d %d", rows, columns);
+
+	if (*rows <= 0 || *columns <= 0) exit(-1);
+}
+
+void AllocMemoryForMatrix(int*** matrix, int* rows, int* columns) 
+{
+	*matrix = (int**)malloc(sizeof(int*) * (*rows));
+	if (!(*matrix)) exit(-1);
+
+	for (int i = 0; i < *rows; i++)
+	{
+		(*matrix)[i] = (int*)malloc(sizeof(int) * (*columns));
+
+		if (!(*matrix)[i])
+		{
+			while (i--)
+				free((*matrix)[i]);
+
+			free(*matrix);
+
+			exit(-1);
+		}
+	}
+}
+
+void InputMatrix(int** matrix, int* rows, int* columns)
+{
+	for (int i = 0; i < *rows; i++)
+	{
+		for (int j = 0; j < *columns; j++)
+		{
+			printf(" Введите [%d][%d] элемент матрицы:\n ", i + 1, j + 1);
+			scanf_s("%d", &matrix[i][j]);
+		}
+	}
+}
+
+void PrintMatrix(int** matrix, int* rows, int* columns, const char* matrixTitle)
+{
+	printf("\n Вывод элементов %s матрицы: \n", matrixTitle);
+	for (int i = 0; i < *rows; i++)
+	{
+		for (int j = 0; j < *columns; j++)
+			printf("%4d", matrix[i][j]);
+
+		printf("\n");
+	}
+}
+
+void FreeMatrixMemory(int** matrix, int* rows, int* columns)
+{
+	for (int i = 0; i < *rows; i++)
+		free(matrix[i]);
+	free(matrix);
+
+	matrix = NULL;
+}
+
+void CalculateAndPrintMatrixSums(int** matrix, int* rows, int* columns, bool const isRowSum) 
+{
+	for (int idx = 0; idx < (isRowSum ? *rows : *columns); idx++) {
+		
+		int negativeSum = 0;
+		int positiveSum = 0;
+
+		for (int subIdx = 0; subIdx < (isRowSum ? *columns : *rows); subIdx++) {
+			int value = isRowSum ? matrix[idx][subIdx] : matrix[subIdx][idx];
+
+			if (value < 0)
+				negativeSum += value;
+			else
+				positiveSum += value;
+		}
+
+		if (isRowSum) 
+		{
+			printf("\n Сумма отрицательных элементов в %d строке = %d \n", idx + 1, negativeSum);
+			printf(" Сумма положительных элементов в %d строке = %d \n", idx + 1, positiveSum);
+		}
+		else 
+		{
+			printf("\n Сумма отрицательных элементов в %d столбце = %d \n", idx + 1, negativeSum);
+			printf(" Сумма положительных элементов в %d столбце = %d \n", idx + 1, positiveSum);
+		}
+	}
+}
